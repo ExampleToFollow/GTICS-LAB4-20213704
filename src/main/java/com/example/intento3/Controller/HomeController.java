76708d6bd253cc67bca4cfbec6057c24eba61c35
Hineill <hineill.cespedes@pucp.edu.pe>
2024-04-25 19:54:41 -0500

    Botones y controllers

--- src/main/java/com/example/intento3/Controller/HomeController.java
+++ src/main/java/com/example/intento3/Controller/HomeController.java
@@ -1,4 +1,25 @@
 package com.example.intento3.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+    @GetMapping("/Menu")
+    public String inicioMenu(){
+        return "principal";
+    }
+    @GetMapping("/verEmpleados")
+    public String verEmpleados(){
+        return "listarEmpleados";
+    }
+    @GetMapping("/verHistorial")
+    public String verHistorial(){
+        return "listarHistorial";
+    }
+    @GetMapping("/verReporte")
+    public String verReporte(){
+        return "listarReporte";
+    }
+
 }
